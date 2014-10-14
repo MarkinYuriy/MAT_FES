8e7f3b3f4d5ebb70825971b406687a20249314a5
demoniskatel <demoniskatel@gmail.com>
2014-10-14 12:13:23 +0300

    @Scope("session")

--- src/src/controller/MatAppl.java
+++ src/src/controller/MatAppl.java
@@ -14,6 +14,7 @@ import mat.Person;
 import mat.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@Scope("session")
 @RequestMapping({"/"})
 public class MatAppl {
 	String m_name;
